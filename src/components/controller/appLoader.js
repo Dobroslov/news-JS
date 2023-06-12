617b98346cfdebfdcaa77a4b2bd3f4beda44ebb7
Vladimir Arshinnikov <lordofrain7@gmail.com>
2023-06-12 21:25:54 +0300

    feat: add api key

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '66ac5df8ae8e4e2cbb8fa4670890357d',
         });
     }
 }
